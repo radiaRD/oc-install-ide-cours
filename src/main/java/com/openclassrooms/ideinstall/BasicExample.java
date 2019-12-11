82c5bcf8766b7fd4560104c5e8f12a12450e6233
radiaRD <radia.madji@gmail.com>
2019-12-11 10:14:01 +0100

    tester l'envoi d'une branche sur github

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -1,11 +1,31 @@
 package com.openclassrooms.ideinstall;
 
+import com.hemebiotech.analytics.ReadSymptomDataFromFile;
+import com.hemebiotech.analytics.SymptomeAnalyse;
+
 public class BasicExample {
 
 	public static void main(String[] args) {
-		int i;
-		for (i = 0; i < 7; i++) {
-			System.out.println("Hello bugs !");
+		// String filepath =
+		// "C:\\Users\\USER\\eclipse-workspace\\Project_1Java_Application
+		// bug\\Project02Eclipse\\symptoms.txt";
+
+		ReadSymptomDataFromFile file = new ReadSymptomDataFromFile(filepath);
+
+		// System.out.println(file.GetSymptoms());
+
+		SymptomeAnalyse hm = new SymptomeAnalyse(file.GetSymptoms());
+
+		hm.occurencesSortedSymptoms();
+
+		for (int i = 0; i < hm.occurencesSortedSymptoms().size(); i++) {
+
+			System.out.println(hm.occurencesSortedSymptoms().keySet().toArray()[i] + " = "
+					+ hm.occurencesSortedSymptoms().values().toArray()[i]);
+
 		}
+
+		SymptomeAnalyse result = new SymptomeAnalyse(hm.occurencesSortedSymptoms());
+		result.writeSymptoms();
 	}
 }
